0ed399c34ff0dda6941ef49ec3fabb08c3293385
tadesanmi <tijesunimiadesanmi@gmail.com>
2022-09-10 00:15:59 +0100

    Assignment_5
    Assignment_5

---
+++ missingnumber.py
@@ -0,0 +1,12 @@
+# A program to find missing numbers in a set of given values
+def findMissingNumbers(n):
+    numbers = set(n)
+    length = len(n)
+    output = []
+    for i in range(1, n[-1]):
+        if i not in numbers:
+            output.append(i)
+    return output
+    
+listOfNumbers = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 13, 14, 16]
+print(findMissingNumbers(listOfNumbers))
